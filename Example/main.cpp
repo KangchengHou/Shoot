22160dceb18fc682b3d7d6c260a9eafccb166d70
Kangcheng Hou <kangchenghou@gmail.com>
2017-05-21 13:33:50 +0800

    fix bugs for macos and delete file

--- Example/main.cpp
+++ Example/main.cpp
@@ -46,6 +46,7 @@ int main()
     // Init GLFW
     glfwInit();
     // Set all the required options for GLFW
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
