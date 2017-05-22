2750f7939c4bacaa0ff49ffcc871bc6a1df08c08
Kangcheng Hou <kangchenghou@gmail.com>
2017-05-22 16:51:30 +0800

    modify file structure

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/xiaoyanzi/Desktop/Shoot
+# Install script for directory: /home/david/Projects/CG/Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -35,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/xiaoyanzi/Desktop/Shoot/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/david/Projects/CG/Project/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
